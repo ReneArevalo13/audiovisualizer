b40de64fbfcadde61d0fb5cdb340ec9a8560751a
renearevalo13 <reneorlandoarevalo@gmail.com>
2025-03-05 17:33:52 -0800

    hello world to understand miniaudio.c

---
+++ engine_hello_world.c
@@ -0,0 +1,9 @@
+/*
+   Example to demonstrate how to initialize an audio engine and play a sound.
+   This plays a sound specified on the command line.
+*/
+
+#inlcude "../miniaudio.c"
+#include <stdio.h>
+
+
